8d053c90bcd42afe6e5b8f7c89e528cd19251360
Chan Meng <Chan.Meng@lincolnuni.ac.nz>
2024-10-24 17:15:10 +1300

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -73,7 +73,7 @@ const Home = () => {
           </p>
         </div>
       </div>
-      <ComputersCanvas />
+      {/*<ComputersCanvas />*/}
       <div className="absolute bottom-10 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
